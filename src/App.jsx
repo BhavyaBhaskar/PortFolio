138e2492e828139180112b712d2b3b863407ba12
BhavyaBhaskar <bhavyabhaskar04@gmail.com>
2024-01-05 18:00:44 +0530

    commit_

--- src/App.jsx
+++ src/App.jsx
@@ -388,7 +388,10 @@ function App() {
             </div>
             <div className="details-container color-container">
               <div className="article-container">
-                <div className="project-img"></div>
+                <div
+                  className="project-img"
+                  // style={{ paddingTop: "10px" }}
+                ></div>
               </div>
               <h2 className="experience-sub-title project-title">
                 Student Exam Registration System
